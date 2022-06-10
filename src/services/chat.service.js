06acd6066ddf35871faea1d813559cdde795ae40
Pham Minh Hoang Nam <efert269@gmail.com>
2022-06-11 01:28:35 +0700

    chat feature with socket and redis

--- src/services/chat.service.js
+++ src/services/chat.service.js
@@ -1,9 +1,17 @@
 class SocketService {
 
     // connection
-    connection(socket) {
+    async connection(socket) {
+        // lưu user id vào redis với value là socket id
+        await _redis.SADD(socket.user._id, socket.id)
+
+        // // test log data
+        // const data = await _redis.SMEMBERS(socket.user._id)
+        // console.log("data ", data);
+
         socket.on("disconnect", () => {
-            console.log("a user disconnected socket ", socket.id);
+            // xoá socket id trong db
+            _redis.SREM(socket.user._id, socket.id)
         })
     }
 }
