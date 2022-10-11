f1004f761e704f9afc6ba0f32d641b5999c36418
otis pham <otis@vinova.com.sg>
2022-10-11 22:58:00 +0700

    maintaince app

--- server.js
+++ server.js
@@ -1,40 +1,37 @@
-const app = require('./app');
-const port = process.env.PORT || 3000
-const passport = require('./src/middlewares/passport.middleware')
-const SocketService = require('./src/services/chat.service')
-const redis = require('redis');
-const PORT_REDIS = process.env.PORT_REDIS
-const redis_client = redis.createClient(PORT_REDIS)
+const app = require("./app");
+const port = process.env.PORT || 3000;
+const passport = require("./src/middlewares/passport.middleware");
+const SocketService = require("./src/services/chat.service");
+const redis = require("redis");
+const redis_client = redis.createClient({ url: process.env.REDIS_URI });
 
-global._redis = redis_client
+global._redis = redis_client;
 
 async function ConnectRedis() {
-    _redis.on('error', (err) => console.log('Redis Client Error', err));
-    await _redis.connect()
+  _redis.on("error", (err) => console.log("Redis Client Error", err));
+  await _redis.connect();
+  console.log("Redis Connected");
 }
 
-const dev = app.get('env') !== 'production';
+const dev = app.get("env") !== "production";
 if (!dev) {
-    ConnectRedis()
+  ConnectRedis();
 }
 
+const server = app.listen(port, () =>
+  console.log("> Server is up and running on port : " + port)
+);
 
-
-
-const server = app.listen(port, () => console.log('> Server is up and running on port : ' + port))
-
-const io = require('socket.io')(server, {
-    allowEIO3: true,
-    cors: {
-        origin: true,
-        methods: ['GET', 'POST'],
-    }
+const io = require("socket.io")(server, {
+  allowEIO3: true,
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"],
+  },
 });
 
-global._io = io
+global._io = io;
 
 global._io.use(passport.jwtAuthenticationSocket);
 
-global._io.on('connection', SocketService.connection)
-
-
+global._io.on("connection", SocketService.connection);
